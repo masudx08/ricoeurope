e91c9d1be0e418750599f895dde97b4a2cd09979
masudx08 <promasud225@gmail.com>
2022-04-08 12:01:51 +0600

    header completed

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
 import './header.css'
 import MyNav from './MyNav'
+import  HeaderImg  from '../../assets/img/Characters/headermembers.png'
 export default function Header() {
   return (
     <div className='headerContainer'>
       <MyNav />
+        <div className="container">
+      <Row >
+        <Col md={6} className='mt-5'>
+          <h1 className="highlight-text">
+            Gang of the Meta
+          </h1>
+          <p className='mt-5 mb-5'>NFT P2E game built on the avalanche block-chain.  Attack and defend against rival gangs. Join a Cartel and Dominate Territory.</p>
+          <button className='playbtn'>Play Now</button>
+        </Col>
+        <Col md={6}>
+          <img  src={HeaderImg} alt="" />
+        </Col>
+      </Row>
+        </div>
     </div>
   )
 }
