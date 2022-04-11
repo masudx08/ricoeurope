47bd8791dd9e77ee43ae0d9156dfeb87881abdf0
masudx08 <promasud225@gmail.com>
2022-04-11 13:42:34 +0600

    home  page fully done

--- src/components/Home/Roadmap.js
+++ src/components/Home/Roadmap.js
@@ -9,7 +9,7 @@ export default function Roadmap() {
   return (
     <div className='roadmapCont'>
       <div className="container">
-        <h2 className="highlight-text text-center">Roadmap</h2>
+        <h2 className="highlight-text text-center mt-5 mb-4">Roadmap</h2>
         <Row>
           <Col md={6}>
             <div className="leftImg">
@@ -17,7 +17,7 @@ export default function Roadmap() {
             </div>
           </Col>
           <Col md={6} className='firstSection'>
-            <h4>The Details</h4>
+            <h4 className='mb-3 '>The Details</h4>
             <p>
               Reveal the project details, and a first look at our NFT gaming concept.</p>
             <p>
@@ -44,9 +44,9 @@ export default function Roadmap() {
             </p>
           </Col>
         </Row>
-        <Row>
+        <Row className='secondSectionRow'>
           <Col className='secondSection'>
-            <h4>The Action</h4>
+            <h4 className='mb-3 mx-3'>The Action</h4>
             <p>
               Listing our native tokens on Coin Market Cap and Coin Gecko.
             </p>
@@ -56,15 +56,17 @@ export default function Roadmap() {
               Primary marketing campaign and Inital gangler NFT Pre-Sale</p>
             <p>
               Launch of our P2E game with PVP attack mode.</p>
-            <h4>The Visibility</h4>
+           <div className="visibilitySection">
+           <h4 className='mb-3 mt-4 mx-3'>The Visibility</h4>
             <p>
               Listing NFTs on secondary marketplaces.</p>
             <p>
               Comprehensive marketing campaign to improve visibility.
             </p>
             <p>Release 1st map.</p>
+           </div>
           </Col>
-          <Col>
+          <Col md={6}>
           <div className="rightImg">
             <img src={Babymama} alt="" />
           </div>
@@ -77,14 +79,16 @@ export default function Roadmap() {
             </div>
           </Col>
           <Col className='thirdSection'>
-            <h4>The Growth</h4>
+            <h4 className='mb-3 '>The Growth</h4>
             <p>Release cartel wars game mode.</p>
             <p>Release of our internal marketplace. </p>
             <p>Release Cartel Tournaments.</p>
             <p>Introduction of new characters to mitigate supply of NFT’s.</p>
-            <h4>The Future</h4>
+            <div className="futureSection">
+            <h4 className='mb-3 mt-4'>The Future</h4>
             <p>Start the development of our expansion.</p>
             <p>Listing on CEX.</p>
+            </div>
           </Col>
         </Row>
       </div>
