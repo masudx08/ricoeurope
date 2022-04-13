72a84f5b5d7ccc5a28e614d3196ed81426434cb3
masudx08 <promasud225@gmail.com>
2022-04-13 22:04:20 +0600

    store done

--- src/pages/Store/Store.js
+++ src/pages/Store/Store.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import "./store.css";
-import Babymama from '../../assets/img/Characters/png/babymama.png'
-import Thief from '../../assets/img/Characters/png/thief.png'
-import Mansion from '../../assets/img/Characters/png/gang_leader_m.png'
-import Plot from '../../assets/img/Characters/png/member.png'
-import Police from '../../assets/img/Characters/png/police.png'
-import Coin from '../../assets/img/Icon/coin.png'
+import Babymama from "../../assets/img/Characters/png/babymama.png";
+import Thief from "../../assets/img/Characters/png/thief.png";
+import Mansion from "../../assets/img/Characters/png/gang_leader_m.png";
+import Plot from "../../assets/img/Characters/png/member.png";
+import Police from "../../assets/img/Characters/png/police.png";
+import Coin from "../../assets/img/Icon/coin.png";
+
+import { FiSearch } from "react-icons/fi";
+
 export default function Store() {
   const datas = [
     {
@@ -44,7 +47,7 @@ export default function Store() {
       level: 8,
       amount: 3.3,
     },
-    
+
     {
       img: Babymama,
       name: "Baby mama",
@@ -80,7 +83,6 @@ export default function Store() {
       level: 8,
       amount: 3.3,
     },
-    
   ];
   return (
     <div className="storeCont">
@@ -90,10 +92,15 @@ export default function Store() {
             <h1>STORE</h1>
           </Col>
           <Col>
-           <div className='text-end'>
-           <p>Search Land Plot No Make Offer</p>
-            <input type="text" placeholder="889" />
-           </div>
+            <div className="text-end">
+              <p>Search Land Plot No Make Offer</p>
+              <div style={{ position: "relative" }}>
+                <input className="topInput" type="text" placeholder="889" />
+                <div className="svgCont">
+                  <FiSearch />
+                </div>
+              </div>
+            </div>
           </Col>
         </Row>
         <div className=" filterArea">
@@ -109,32 +116,46 @@ export default function Store() {
           <p className="selectedFilter">Sort By</p>
         </div>
         <div>
-          <Row style={{rowGap:'50px',  margin:'auto'}}>
-            {
-              datas.map((data, index)=>{
-                return (
-                  <Col className='mycol' md={2} key={index}>
-                    <div className='imgPart'>
-                      <img src={data.img} alt="" />
+          <Row style={{ rowGap: "50px", margin: "auto" }}>
+            {datas.map((data, index) => {
+              return (
+                <Col className="mycol" md={2} key={index}>
+                  <div className="imgPart">
+                    <img src={data.img} alt="" />
+                  </div>
+                  <div className="contentPart">
+                    <p>
+                      {data.name} {data.id && `#${data.id}`}
+                    </p>
+                    <p style={{ fontSize: "15px" }}>
+                      {data.breedCount && `Breed Count : ${data.breedCount}`}
+                      {data.level && `Level : ${data.level}`}
+                      {data.accomodation &&
+                        `Accomodation : ${data.accomodation}`}
+                      {data.makeover && `Make Over : ${data.makeover}`}
+                    </p>
+                    <div
+                      className="d-flex align-items-center"
+                      style={{ fontSize: "15px" }}
+                    >
+                      <img
+                        style={{ height: "18px", marginRight: "5px" }}
+                        src={Coin}
+                        alt=""
+                      />
+                      <p>{data.amount}</p>
                     </div>
-                    <div className='contentPart'>
-                      <p>{data.name} {data.id && `#${data.id}`}</p>
-                      <p style={{fontSize:'15px'}}>
-                        {data.breedCount && `Breed Count : ${data.breedCount}`}
-                        {data.level && `Level : ${data.level}`}
-                        {data.accomodation && `Accomodation : ${data.accomodation}`}
-                        {data.makeover && `Make Over : ${data.makeover}`}
-                      </p>
-                      <div className='d-flex align-items-center' style={{fontSize:'15px'}}>
-                        <img  style={{height:'18px',  marginRight:'5px'}} src={Coin} alt="" />
-                        <p>{data.amount}</p>
-                      </div>
-                    </div>
-                  </Col>
-                )
-              })
-            }
+                  </div>
+                </Col>
+              );
+            })}
           </Row>
+          <div className="bottomBtns">
+            <button className='mybtn mybtn-p'>ADD</button>
+            <button className='outlinebtn'>SELECT</button>
+            <button className='outlinebtn'>PURCHASE</button>
+            <button className='notbtn'>My Offers</button>
+          </div>
         </div>
       </div>
     </div>
